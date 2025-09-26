e868b04831905d1dcd2b26ef932426a9f534ed2f
Júlio Mühlbauer <juliowernermm@gmail.com>
2025-09-26 13:09:30 -0300

    add nitro

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,11 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
+import { nitroV2Plugin } from "@tanstack/nitro-v2-vite-plugin";
 
 const config = defineConfig({
   plugins: [
+    nitroV2Plugin(),
     paraglideVitePlugin({
       project: "./project.inlang",
       outdir: "./src/paraglide",
